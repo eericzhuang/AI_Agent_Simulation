9328818ef12d3953882a286222c6b05b056ad077
eericzhuang <166406604+eericzhuang@users.noreply.github.com>
2026-04-05 01:05:25 -0500

    Added simple AI Agent

---
+++ agent.py
@@ -0,0 +1,23 @@
+# self designed AI-agent using Claude Sonnet
+
+# import API key
+from dotenv import load_dotenv
+load_dotenv()
+
+import anthropic
+
+client = anthropic.Anthropic()
+
+# chat history
+messages = []
+
+# add to chat history
+user_input = input("you: ")
+messages.append({"role": "user", "content": user_input})
+
+# generate response from Claude Sonnet model
+response = client.messages.create(
+    model="claude-sonnet-4-20250514", max_tokens=1024, messages=messages # input full chat history
+)
+
+print("Claude: ", response.content[0].text)
